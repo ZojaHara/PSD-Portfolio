30a4972e11625bb802b5874aaac8ebbca8bdc558
ZojaHara <k.harasimiuk@gmail.com>
2018-01-25 16:56:46 +0100

    header section finished

---
+++ js/menu-script.js
@@ -0,0 +1,27 @@
+;(function($) {
+
+$(document).ready(function() {
+
+  var hamburger = $("#hamburger");
+  var nav = $(".nav-group");
+  var ham1 = $('.hamburger-dash:nth-of-type(1)');
+  var ham2 = $('.hamburger-dash:nth-of-type(2)');
+  var ham3 = $('.hamburger-dash:nth-of-type(3)');
+
+    hamburger.on('click', function() {
+      // $(this).toggleClass("move-hamb");
+      ham1.toggleClass("rot1");
+      ham2.toggleClass("opacity");
+      ham3.toggleClass("rot2");
+      nav.toggleClass('move-nav');
+    });
+
+  //   $(window).resize(function(){
+  //     nav.removeClass('move-nav');
+  // });
+
+
+});
+
+
+})(jQuery);
