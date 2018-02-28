bd0a112d3e76fc505e6bde45a58ef8608baa46ec
ZojaHara <k.harasimiuk@gmail.com>
2018-02-28 18:34:33 +0100

    header animations further steps

--- js/menu-script.js
+++ js/menu-script.js
@@ -8,12 +8,50 @@ $(document).ready(function() {
   var ham2 = $('.hamburger-dash:nth-of-type(2)');
   var ham3 = $('.hamburger-dash:nth-of-type(3)');
 
+  var mainHeading = $(".main-heading");
+  // var initialPos = mainHeading.css("padding-left");
+  var newClick = true;
+
+  //
+  //
+  // var headingWidth = mainHeading.innerWidth();
+  // var groupWidth = $(".header-parts").innerWidth();
+  //
+  // console.log(groupWidth);
+  // console.log(headingWidth);
+
     hamburger.on('click', function() {
       ham1.toggleClass("rot1");
       ham2.toggleClass("opacity");
       ham3.toggleClass("rot2");
       nav.toggleClass('move-nav');
+
+
+      $(window).resize(function(){
+        if ($(window).width() <= 1023){
+          mainHeading.css("align-self","center");
+        } else {
+          mainHeading.css("align-self","flex-start");
+          if(nav.hasClass('move-nav')) {
+            nav.toggleClass('move-nav');
+          }
+        }
+
+
+      });
+      if ($(window).width() > 1024){
+        var newPos = newClick ? "35%" : "0%";
+
+          mainHeading.animate({
+            paddingLeft: newPos
+          }, 100, "linear");
+
+          newClick = !newClick;
+
+      }
+
     });
+
 
   //   $(window).resize(function(){
   //     nav.removeClass('move-nav');
@@ -49,6 +87,8 @@ $(document).ready(function() {
             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           }
+
+          nav.toggleClass('move-nav');
         });
       }
     }
